b59d19577b2783dab80a6aeb248e4b96c32d3e2e
James Ashby <James.Ashby@Colorado.EDU>
2022-05-04 12:41:16 -0600

    Add insert task

--- oit_ds_prefect_tools/database.py
+++ oit_ds_prefect_tools/database.py
@@ -4,6 +4,7 @@ import prefect
 import cx_Oracle
 from prefect import task
 import pandas as pd
+import numpy as np
 
 from . import util
 
@@ -16,6 +17,23 @@ def sql_extract(sql_query: str, connection_info: dict) -> pd.DataFrame:
 
     return oracle_sql_extract(sql_query, connection_info)
 
+@task
+def insert(
+        dataframe: pd.DataFrame,
+        table_identifier: str,
+        connection_info: dict,
+        replace_existing: bool =False) -> pd.DataFrame:
+    """Takes a dataframe and table identifier (schema.table) and appends the data into that table.
+    If kill_and_fill is true, deletes all rows from thet able before inserting. Dataframe columns
+    must match table column names.
+    """
+
+    return oracle_insert(dataframe, table_identifier, connection_info, replace_existing)
+
+def _sql_error(sql_query, offset):
+    line_no = len(sql_query[:offset].split('\n'))
+    line = sql_query[:offset].split('\n')[-1] + '█' + sql_query[offset:].split('\n')[0]
+    return f'Line {line_no}: {line[:100]}'
 
 # Oracle functions
 
@@ -41,30 +59,79 @@ def oracle_sql_extract(sql_query: str, connection_info: dict) -> pd.DataFrame:
     _make_oracle_dsn(connection_info)
     if 'encoding' not in connection_info:
         connection_info['encoding'] = 'UTF-8'
-    conn = cx_Oracle.connect(**connection_info)
-    try:
-        host = conn.dsn.split('HOST=')[1].split(')')[0]
-    except IndexError:
-        host = 'UNKNOWN'
-    sql_snip = ' '.join(sql_query.split())[:50]
-    try:
-        data = pd.read_sql_query(sql_query, conn)
-    except pd.io.sql.DatabaseError:
-        # Can't get detailed error information from this, so reproduce with oracle library
+    with cx_Oracle.connect(**connection_info) as conn:
         try:
-            conn.cursor().execute(sql_query)
-        except cx_Oracle.DatabaseError as exc:
+            host = conn.dsn.split('HOST=')[1].split(')')[0]
+        except IndexError:
+            host = 'UNKNOWN'
+        sql_snip = ' '.join(sql_query.split())[:50]
+        try:
+            data = pd.read_sql_query(sql_query, conn)
+        except pd.io.sql.DatabaseError:
+            # Can't get detailed error information from this, so reproduce with oracle library
             try:
-                offset = exc.args[0].offset
-            except (IndexError, AttributeError):
-                pass
-            else:
-                line_no = len(sql_query[:offset].split('\n'))
-                line = sql_query[:offset].split('\n')[-1] + '█' + sql_query[offset:].split('\n')[0]
-                prefect.context.get('logger').error(
-                    f'Oracle database error! {exc}\nLine {line_no}: {line[:100]}')
+                conn.cursor().execute(sql_query)
+            except cx_Oracle.DatabaseError as exc:
+                try:
+                    offset = exc.args[0].offset
+                except (IndexError, AttributeError):
+                    pass
+                else:
+                    prefect.context.get('logger').error(
+                        f'Oracle: Database error - {exc}\n{_sql_error(sql_query, offset)}')
+                raise
             raise
-        raise
-    prefect.context.get('logger').info(f"Read {len(data.index)} rows from {host}: {sql_snip}")
+    prefect.context.get('logger').info(
+        f"Oracle: Read {len(data.index)} rows from {host}: {sql_snip}")
     util.record_source('oracle', host, sum(data.memory_usage()))
     return data
+
+def oracle_insert(
+        dataframe: pd.DataFrame,
+        table_identifier: str,
+        connection_info: dict,
+        kill_and_fill: bool =False) -> pd.DataFrame:
+    """Takes a dataframe and table identifier (schema.table) and inserts the data into that table.
+    If kill_and_fill is true, deletes all rows from thet able before inserting. Dataframe columns
+    must match table column names.
+    """
+
+    batch_size = 500
+    errors = 0
+    insert_sql = (f'INSERT INTO {table_identifier} ({",".join(list(dataframe.columns))}) ' +
+                  f'VALUES ({",".join(":" + i for i in dataframe.columns)})')
+    _make_oracle_dsn(connection_info)
+    if 'encoding' not in connection_info:
+        connection_info['encoding'] = 'UTF-8'
+
+    # Replace NA values with None and turn to list of dicts
+    records = dataframe.fillna(np.nan).replace([np.nan], [None]).to_dict('records')
+
+    with cx_Oracle.connect(**connection_info) as conn:
+        try:
+            host = conn.dsn.split('HOST=')[1].split(')')[0]
+        except IndexError:
+            host = 'UNKNOWN'
+        with conn.cursor() as cursor:
+            if kill_and_fill:
+                cursor.execute('TRUNCATE TABLE :table', [table_identifier])
+
+            # Insert records in batches
+            for start in range(0, len(records), batch_size):
+                to_insert = records[start : start + batch_size]
+                cursor.executemany(insert_sql, to_insert, batcherrors=True)
+                batch_errors = cursor.getbatcherrors()
+                for error in batch_errors[:10 - errors]:
+                    prefect.context.get('logger').error(
+                        f'Oracle: Database error {error.message} while inserting data '
+                        f'{to_insert[error.offset]}')
+                errors += len(batch_errors)
+            conn.commit()
+
+    # Logging
+    if errors > 10:
+        prefect.context.get('logger').error(
+            f'Oracle: {errors - 10} more database errors while inserting not shown')
+    prefect.context.get('logger').info(
+        f"Oracle: Inserted {len(records) - errors} rows into {table_identifier} on {host}")
+    util.record_sink('oracle', host, sum(dataframe.memory_usage()))
