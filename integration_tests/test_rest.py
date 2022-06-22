699e830354d49a4ed61d26fd13e576a6f91f589b
James Ashby <James.Ashby@Colorado.EDU>
2022-06-22 13:17:37 -0600

    Fix result unpacking

--- integration_tests/test_rest.py
+++ integration_tests/test_rest.py
@@ -17,18 +17,19 @@ class GetTests(unittest.TestCase):
         """Tests the get task with paginated results"""
 
         @task
-        def validate(result):
-            self.assertGreater(len(result), 100)
+        def validate(results):
+            data = [i for result in results for i in result['results']]
+            self.assertGreater(len(data), 500)
 
         def next_page(response):
             return response.json()['next']
 
         with Flow('test') as flow:
             flow.add_task(Parameter('env', default='dev'))
-            result = rest.get(
+            results = rest.get(
                 endpoint='/api/v2/pokemon/?limit=300',
                 next_page_getter=next_page,
                 connection_info=self.connection_info)
-            validate(result)
+            validate(results)
         state = flow.run()
         self.assertFalse(state.is_failed())
