5f84923a2f6c226f1c16c68bb87add2f1e0141c6
singularity3 <chko2775@colorado.edu>
2023-09-21 11:06:52 -0600

    Take object reads outside of conversion context

--- ucb_prefect_tools/util.py
+++ ucb_prefect_tools/util.py
@@ -378,9 +378,12 @@ def run_model(data: pd.DataFrame, model_path:str) -> pd.DataFrame:
     """Reads an R model from an RDS file and runs its associated predict method on a dataframe, returning the predictions"""
 
     with (ro.default_converter + pandas2ri.converter).context():
-        importr("glmnet")
         r_dataframe = ro.conversion.get_conversion().py2rpy(data)
-        model = ro.r.readRDS(model_path)
-        preds = ro.r.predict(model, r_dataframe)
+
+    model = ro.r.readRDS(model_path)
+    preds = ro.r.predict(model, r_dataframe)
+
+    with (ro.default_converter + pandas2ri.converter).context():
         pred_dataframe = ro.conversion.get_conversion().rpy2py(preds)
+
     return pred_dataframe
