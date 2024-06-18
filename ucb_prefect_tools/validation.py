2be36b49fa5a4f5d001283c4c292ce840590f1c4
singularity3 <chko2775@colorado.edu>
2024-06-18 15:19:02 -0600

    formatting, metadata, documentation

--- ucb_prefect_tools/validation.py
+++ ucb_prefect_tools/validation.py
@@ -1,3 +1,9 @@
+"""Functions for validating dataframes and generating schemas for validation.
+
+This uses Pandera, a framework for schematic validation of dataframes. For simpler validations, schemas can be
+represented as a YAML file which can be read in at validation time. If custom checks or complex validations are
+desired, schemas can be generated as Python scripts and provided to the validation task as objects."""
+
 import pandas as pd
 import pandera as pa
 from pandera.io import from_yaml
@@ -16,23 +22,36 @@ def generate_schema(df: pd.DataFrame, to_script=False, path: PathLike | None = N
     schema = pa.infer_schema(df)
     if to_script:
         if not path:
-            raise NotImplementedError("Schemas in script form must be written to a file, please specify a path")
+            raise NotImplementedError(
+                "Schemas in script form must be written to a file, please specify a path"
+            )
         schema.to_script(path)
         return None
     return schema.to_yaml(path)
 
+
 @task
-def validate_dataframe(df: pd.DataFrame, schema: pa.DataFrameSchema = None, yaml_path: PathLike = None, warn=False):
+def validate_dataframe(
+    df: pd.DataFrame,
+    schema: pa.DataFrameSchema = None,
+    yaml_path: PathLike = None,
+    warn=False,
+):
     """Lazily validates target dataframe using a schema or schema file, and throws an error report if violations are detected.
     If warn=True, will instead write the report as a warning to the run logger."""
     if not schema:
         if not yaml_path:
-            raise ValueError("Must provide either a schema or a path to a YAML file containing a schema.")
+            raise ValueError(
+                "Must provide either a schema or a path to a YAML file containing a schema."
+            )
         schema = from_yaml(yaml_path)
     try:
         schema.validate(df, lazy=True)
     except pa.errors.SchemaErrors as exc:
         if warn:
-            get_run_logger().warning("WARNING - Dataframe failed validation:\n" + json.dumps(exc.message, indent=2))
+            get_run_logger().warning(
+                "WARNING - Dataframe failed validation:\n"
+                + json.dumps(exc.message, indent=2)
+            )
         else:
             raise exc
