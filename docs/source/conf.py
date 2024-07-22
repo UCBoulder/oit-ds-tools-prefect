8db9f482ea030990a8374d2e381d763b69a2bc13
James Ashby <james.ashby@colorado.edu>
2024-07-22 14:29:13 -0600

    Add sphinx docs project and update gitignore

---
+++ docs/source/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import sys
+from pathlib import Path
+
+# Path for package
+sys.path.insert(0, Path(__file__).parents[2].resolve().as_posix())
+# Path for custom extension
+sys.path.insert(0, str(Path(__file__).parent.absolute()))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "UCB Prefect Tools"
+copyright = "2024, OIT Data Services"
+author = "OIT Data Services"
+release = "v6.2.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "task_autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+
+# Set up autodoc to include all members
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "special-members": True,
+    "show-inheritance": True,
+}
+
+autosummary_generate = True
+autosummary_imported_members = True
+add_module_names = False
+
+# Sort members by source order
+autodoc_member_order = "bysource"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+Welcome to UCB Prefect Tools documentation!
+===========================================
+
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
+
+   ucb_prefect_tools
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   _autosummary/ucb_prefect_tools
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
